9d606504b53db8645c89ec5fa061cc9477a077f9
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-09-07 10:39:36 +0000

    adding styles

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='container'>
+        <h1>Task Tracker</h1>
+        <button className='btn'>Add</button>
+    </header>
+  )
+}
+
+
+
+export default Header
